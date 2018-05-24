4b7485ee5c63bf94cbbd19e2e2dd84fe1c901885
Ryan Hageman <improv42@gmail.com>
2018-05-24 16:09:02 -0500

    Add markdown parsing * RedCarpet - convert to HTML * CodeRay - syntax highlighting

--- Gemfile
+++ Gemfile
@@ -48,6 +48,10 @@ gem 'simple_form'
 # JavaScript
 gem 'webpacker', '~> 3.5'
 
+# Markdown
+gem 'redcarpet'
+gem 'coderay'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module MarkdownHelper
+  MARKDOWN_OPTIONS = {
+    filter_html: true,
+    prettify: true,
+    link_attributes: {
+      rel: 'nofollow',
+      target: '_blank'
+    }
+  }
+
+  MARKDOWN_EXTENSIONS = {
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    autolink: true,
+    strikethrough: true,
+    space_after_headers: true,
+    superscript: true,
+    underline: true,
+    highlight: true,
+    footnotes: true
+  }.freeze
+
+  class CodeRayTransform < Redcarpet::Render::HTML
+    def block_code(code, language)
+      language ||= :ruby
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def highlight_syntax(options)
+    CodeRayTransform.new(options)
+  end
+
+  def markdown_to_html(text, options, extensions)
+    Redcarpet::Markdown
+      .new(highlight_syntax(options), extensions)
+      .render(text).html_safe
+  end
+
+  def markdown(text)
+    markdown_to_html(text, MARKDOWN_OPTIONS, MARKDOWN_EXTENSIONS)
+  end
+end
