ecdde3d87e32b0b6212bfbba88ccdbea5f2aa58d
Ryan Hageman <improv42@gmail.com>
2018-05-23 22:57:33 -0500

    Add lepturn markdown text editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require editor
+//= require marked
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
