5cfd9b73c03433644ff1cea1dececbdb394fc989
Ryan Hageman <improv42@gmail.com>
2018-05-25 20:26:45 -0500

    Add methods to post model to parse and create tags and categories from raw, comma separated strings.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,56 @@
 # Post Model
 class Post < ApplicationRecord
   attr_accessor :raw_tags, :raw_categories
-  belongs_to :author, class_name: 'User'
-  has_many :post_tags
-  has_many :tags, through: :post_tags
-  has_many :post_categories
-  has_many :categories, through: :post_categories
-
   has_one_attached :main_image
+
+  belongs_to :author, class_name: 'User'
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, -> { distinct }, through: :post_tags
+  has_many :post_categories, dependent: :destroy
+  has_many :categories, -> { distinct }, through: :post_categories
+
+  accepts_nested_attributes_for :post_tags, allow_destroy: true
+  accepts_nested_attributes_for :post_categories, allow_destroy: true
+
   has_many_attached :images
 
   validates_presence_of :title, :body, :author_id, :author
+
+  def raw_data_to_array(raw_data)
+    raw_data.split(',').map(&:strip)
+  end
+
+  def parse_raw_tags(data)
+    raw_tags = data[:post][:raw_tags]
+
+    return [] unless raw_tags
+    create_tag_id_array(raw_tags)
+  end
+
+  def create_tag_id_array(raw_tags)
+    get_tag_ids(raw_tags).map { |id| { tag_id: id } }
+  end
+
+  def get_tag_ids(raw_tags)
+    raw_data_to_array(raw_tags).map do |tag|
+      Tag.where(name: tag).first_or_create.id
+    end
+  end
+
+  def parse_raw_categories(data)
+    raw_categories = data[:post][:raw_categories]
+
+    return [] unless raw_categories
+    create_category_id_array(raw_categories)
+  end
+
+  def create_category_id_array(raw_categories)
+    get_category_ids(raw_categories).map { |id| { category_id: id } }
+  end
+
+  def get_category_ids(raw_categories)
+    raw_data_to_array(raw_categories).map do |category|
+      Category.where(name: category).first_or_create.id
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let(:post) { build_stubbed(:post) }
   it 'has a valid factory' do
     expect(create(:post)).to be_valid
   end
@@ -17,4 +18,114 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_presence_of(:author_id) }
   it { is_expected.to validate_presence_of(:author) }
+
+  describe 'raw_data_to_array' do
+    it 'splits a raw data string into an array' do
+      data_string = 'first tag, second, third cat e gory here'
+
+      expect(post.raw_data_to_array(data_string).count).to eq(3)
+    end
+  end
+
+  context 'tags' do
+    let(:data) { { post: { raw_tags: 'tag one, tag two' } } }
+
+    describe 'parse_raw_tags' do
+      let(:tag_array) { post.parse_raw_tags(data) }
+
+      it 'creates new tags from raw data' do
+        first_tag_id = tag_array.first[:tag_id]
+
+        expect(Tag.find(first_tag_id)[:name]).to eq('tag one')
+      end
+
+      it 'returns an array filled with :tag_id hashes' do
+        expect(tag_array.first.include?(:tag_id)).to be(true)
+      end
+
+      it 'returns an empty array when no tag data is given' do
+        no_tag_data = { post: {} }
+
+        expect(post.parse_raw_tags(no_tag_data)).to eq([])
+      end
+    end
+
+    describe 'create_tag_id_array' do
+      let(:raw_tag_string) { 'first tag, second tag' }
+      let(:tag_id_array) { post.create_tag_id_array(raw_tag_string) }
+
+      it 'creates an array of tag ids' do
+        expect(tag_id_array.first.include?(:tag_id)).to be(true)
+      end
+
+      it 'creates two tags' do
+        expect(tag_id_array.count).to eq(2)
+      end
+    end
+
+    describe 'get_tag_ids' do
+      let(:raw_tag_string) { 'first tag here, second, third tag' }
+      let(:tag_id_array) { post.get_tag_ids(raw_tag_string) }
+
+      it 'returns 3 tag ids' do
+        expect(tag_id_array.count).to eq(3)
+      end
+
+      it 'returns valid tag ids' do
+        expect(Tag.find(tag_id_array.first).class).to eq(Tag)
+      end
+    end
+  end
+
+  context 'categories' do
+    let(:data) { { post: { raw_categories: 'category one, category two' } } }
+
+    describe 'parse_raw_categories' do
+      let(:category_array) { post.parse_raw_categories(data) }
+
+      it 'creates new categories from raw data' do
+        first_category_id = category_array.first[:category_id]
+
+        expect(Category.find(first_category_id)[:name]).to eq('category one')
+      end
+
+      it 'returns an array filled with :category_id hashes' do
+        expect(category_array.first.include?(:category_id)).to be(true)
+      end
+
+      it 'returns an empty array when no category data is given' do
+        no_category_data = { post: {} }
+
+        expect(post.parse_raw_categories(no_category_data)).to eq([])
+      end
+    end
+
+    describe 'create_category_id_array' do
+      let(:raw_category_string) { 'first category, second category' }
+      let(:category_id_array) do
+        post.create_category_id_array(raw_category_string)
+      end
+
+      it 'creates an array of category ids' do
+        expect(category_id_array.first.include?(:category_id)).to be(true)
+      end
+
+      it 'creates two categories' do
+        expect(category_id_array.count).to eq(2)
+      end
+    end
+
+    describe 'get_category_ids' do
+      let(:raw_category_string) { 'first cat e gory, second, third category' }
+      let(:category_id_array) { post.get_category_ids(raw_category_string) }
+
+      it 'returns 3 category ids' do
+        expect(category_id_array.count).to eq(3)
+      end
+
+      it 'returns valid category ids' do
+        expect(Category.find(category_id_array.first).class).to eq(Category)
+      end
+    end
+  end
 end
