a52438a9c0e14adaa3efd9b6f28bd22ddb03eb30
Ryan Hageman <improv42@gmail.com>
2018-10-03 10:06:23 -0500

    Refactor Post Factory

--- spec/factories/blog-admin/posts.rb
+++ spec/factories/blog-admin/posts.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
+# Post Factory
 FactoryBot.define do
   sequence(:title) { |n| "Post Title #{n}" }
-  
+
   factory :post do
     author
     title
     body { 'I like to move it move it! You like to, MOVE IT!' }
   end
+end
 
+# Post States
+FactoryBot.define do
   trait :draft do
     aasm_state { 'draft' }
   end
@@ -20,7 +24,10 @@ FactoryBot.define do
   trait :hidden do
     aasm_state { 'hidden' }
   end
+end
 
+# Post Visibility
+FactoryBot.define do
   trait :visible do
     publish_date { Time.current - 1.day }
   end
@@ -28,7 +35,10 @@ FactoryBot.define do
   trait :unreleased do
     publish_date { Time.current + 2.days }
   end
+end
 
+# Post with tags
+FactoryBot.define do
   trait :with_tags do
     after(:create) do |post|
       tag_list = create_list(:tag, 3)
@@ -45,6 +55,16 @@ FactoryBot.define do
       create(:post_tag, post: post, tag: evaluator.tag)
     end
   end
+end
+
+# Post with Categories
+FactoryBot.define do
+  trait :with_categories do
+    after(:create) do |post|
+      cat_list = create_list(:category, 3)
+      cat_list.each { |cat| create(:post_category, post: post, category: cat) }
+    end
+  end
 
   trait :with_specific_category do
     transient do
@@ -53,13 +73,6 @@ FactoryBot.define do
 
     after(:create) do |post, evaluator|
       create(:post_category, post: post, category: evaluator.category)
-    end
-  end
-
-  trait :with_categories do
-    after(:create) do |post|
-      cat_list = create_list(:category, 3)
-      cat_list.each { |cat| create(:post_category, post: post, category: cat) }
     end
   end
 end
