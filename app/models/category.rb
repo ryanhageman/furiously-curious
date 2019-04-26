8c1c53151eb2d10f3fa362db7bf038844e8e076f
Ryan Hageman <improv42@gmail.com>
2019-04-26 16:30:59 -0500

    Add Category#search_names

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,4 +8,8 @@ class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   before_validation { self.name &&= name.downcase }
+
+  def self.search_names(name)
+    where('name ILIKE ?', "%#{name}%")
+  end
 end
