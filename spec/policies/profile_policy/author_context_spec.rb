acfb70502485c669115a7850077bdb6f0f920c76
Ryan Hageman <improv42@gmail.com>
2019-05-09 13:19:02 -0500

    Add author context to profile policy tests.

--- spec/policies/profile_policy/author_context_spec.rb
+++ spec/policies/profile_policy/author_context_spec.rb
@@ -5,20 +5,10 @@ require 'rails_helper'
 RSpec.describe ProfilePolicy do
   subject { described_class.new(user, profile) }
 
-  let(:user) { create(:user) }
+  let(:user) { create(:user, :author) }
   let(:profile) { create(:profile, user_id: user.id) }
 
-  context 'any user creating a new profile' do
-    it { is_expected.to permit_new_and_create_actions }
-  end
-
-  context 'any user accessing their own profile' do
-    it { is_expected.to permit_action(:show) }
-    it { is_expected.to permit_edit_and_update_actions }
-    it { is_expected.to permit_action(:destroy) }
-  end
-
-  context "user accessing someone else's profile" do
+  context "user or author accessing someone else's profile" do
     let(:user2) { create(:user) }
     let(:profile) { create(:profile, user_id: user2.id) }
 
