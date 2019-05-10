cbe79dcb710bad40b03ae81490443769e5a439b2
Ryan Hageman <improv42@gmail.com>
2019-05-10 13:25:10 -0500

    Add static pages controller and about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get 'about', to: 'pages#about', as: 'about'
+
   namespace :accounts do
     resources :post_images
     resources :profiles
